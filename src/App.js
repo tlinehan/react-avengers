3971d3cfa29a4f4a5319aaf1c45fb9e09ebf77a8
-tristan- <tristan.linehan@gmail.com>
2018-06-18 14:39:21 -0700

    avengers app complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import avengers from './avengers' ; 
 
 class App extends Component {
+  constructor() {
+    super() ;
+    this.state = {
+      avengers: avengers
+    } ;
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Avengers: by Tristan Linehan</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+          {this.state.avengers.map(avenger => {
+            return(
+              <p key="avenger.id">
+                {avenger.name}
+              </p>              
+            )
+          })}
       </div>
     );
   }
